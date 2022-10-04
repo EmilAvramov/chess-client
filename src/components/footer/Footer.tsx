c4286c9dd31e754f080029af27c151fc04252d8f
EmilAvramov <e_avramov@zoho.eu>
2022-10-04 11:41:16 +0300

    clean up, package adjustment, added components

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+        <div>
+            <p>Back to Top</p>
+            <ul>
+                <li>Contacts</li>
+                <li>Support</li>
+                <li>Placeholder</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+function Header() {
+	return (
+		<ul>
+			<li>Home</li>
+			<li>Chess</li>
+			<li>Login</li>
+			<li>Register</li>
+		</ul>
+	);
+}
+
+export default Header;
