2573702234891a93cbf51950401ff118b8902adf
EmilAvramov <e_avramov@zoho.eu>
2022-10-05 14:34:26 +0300

    added some mixings and improved header styles

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,16 +1,53 @@
 import { NavLink } from 'react-router-dom';
-import styles from './header.module.scss'
+import styles from './header.module.scss';
 
 const Header = () => {
 	return (
 		<header className={styles['header__wrapper']}>
-			<ul className={styles['header__list']}>
-				<li><NavLink to={'/'}>Home</NavLink></li>
-				<li><NavLink to={'chess'}>Chess</NavLink></li>
-				<li><NavLink to={'login'}>Login</NavLink></li>
-				<li><NavLink to={'register'}>Register</NavLink></li>
-				<li><NavLink to={'about'}>About</NavLink></li>
-			</ul>
+			<nav>
+				<ul className={styles['header__list']}>
+					<li className={styles['header__list_item']}>
+						<NavLink
+							to={'/'}
+							className={styles['header__list_nav']}
+						>
+							Home
+						</NavLink>
+					</li>
+					<li className={styles['header__list_item']}>
+						<NavLink
+							to={'chess'}
+							className={styles['header__list_nav']}
+						>
+							Chess
+						</NavLink>
+					</li>
+					<li className={styles['header__list_item']}>
+						<NavLink
+							to={'login'}
+							className={styles['header__list_nav']}
+						>
+							Login
+						</NavLink>
+					</li>
+					<li className={styles['header__list_item']}>
+						<NavLink
+							to={'register'}
+							className={styles['header__list_nav']}
+						>
+							Register
+						</NavLink>
+					</li>
+					<li className={styles['header__list_item']}>
+						<NavLink
+							to={'about'}
+							className={styles['header__list_nav']}
+						>
+							About
+						</NavLink>
+					</li>
+				</ul>
+			</nav>
 		</header>
 	);
 };
