469d6a63bd469aaab7966eb0ec9f8e3b37e70a38
EmilAvramov <e_avramov@zoho.eu>
2022-10-25 15:47:02 +0300

    added bcrypt and hash function

---
+++ src/functions/hasher.function.ts
@@ -0,0 +1,11 @@
+import { hash, genSalt, compare } from 'bcrypt';
+import { saltRounds } from '../helpers/misc/config';
+
+export const hashPassword = async (password: string) => {
+	const salt = await genSalt(saltRounds);
+	return await hash(password, salt);
+};
+
+export const matchPasswords = async (stored: string, current: string) => {
+    return await compare(stored, current)
+}
